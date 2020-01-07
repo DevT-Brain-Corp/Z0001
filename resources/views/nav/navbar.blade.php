330b21a12d2129764dc9cce817150c5cf936e6d8
alfian27 <alfian172410107@gmail.com>
2020-01-07 22:07:15 +0700

    detail fix

--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap-grid.min.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap-reboot.min.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-techmarket.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/slick.css')}}" media="all" />
+         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/techmarket-font-awesome.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/slick-style.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.min.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}" media="all" />
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/css/colors/blue.css')}}" media="all" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}" media="all" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}" media="all" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap-grid.min.css') }}" media="all" />
@@ -1449,6 +1460,19 @@
 <script type="text/javascript" src="{{ asset('assets/js/pace.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/js/slick.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('assets/js/scripts.js') }}"></script>
-
+ <script type="text/javascript" src="{{asset('assets/js/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/tether.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/jquery-migrate.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/hidemaxlistitem.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/jquery-ui.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/hidemaxlistitem.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/jquery.easing.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/scrollup.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/jquery.waypoints.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/waypoints-sticky.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/pace.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/slick.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('assets/js/scripts.js')}}"></script>
 </body>
 </html>
